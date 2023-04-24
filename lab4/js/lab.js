9c45a32be81166369253f866a09cbc097b8fb268
skylarharp <skylarh316@gmail.com>
2023-04-24 14:16:07 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,16 +1,60 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
-
-// Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+
+// Rock Paper Scissors
+// Beat the computer
+// Author: Skylar Harp
+// Date:04/24/2023
+
+//Outline
+//Creating a Rock-Paper-Scissors game:
+//1. Print 3 options
+//2. Accept user input
+//3. Randomize computer input
+//6. Test of win or draw condition
+
+
+// Constants
+// Rock > Scissors
+// Paper > Rock
+// Scissprs > Paper
+
+// Functions
+// Print Rock, Paper, Scissors
+   //Loop Generate Computers choice
+       //Ask user for Choice
+   //Loop Play Game
+     //If User Plays Rock and Computer Chose Rock
+       //Display Try Again
+     //If User Plays Rock and Computer Chose Paper
+       //Display you took the L
+       //Prompt try again
+     //If User Plays Rock and Computer Scissors
+       //Display Congratulations! Player Won!
+       //Prompt play again
+     //If User Plays Paper and Computer Paper
+       //Display Try Again
+    //If User Plays Paper and Computer Scissors
+       //Display you took the L
+       //Prompt try again
+     //If User Plays Paper and Computer Rock
+       //Display Congratulations! Player Won!
+       //Prompt play again
+     //If User Plays Scissors and Computer Scissors
+       //Display Try Again
+     //If User Plays Scissors and Computer Rock
+       //Display you took the L
+       //Prompt try again
+     //If User Plays Scissors and Computer Paper
+       //Display Congratulations! Player Won!
+       //Prompt play again
+
+
 
 function main() {
   console.log("Main function started.");
@@ -19,3 +63,23 @@ function main() {
 
 // let's get this party started
 main();
+
+  // How to make a peanut butter and jelly sandwich:
+	//	find a loaf of pre-sliced bread in the pantry 
+	//	find peanut butter in the pantry (I like chunky Skippy)
+	//	find jelly in the fridge (I like raspberry)
+	//	find a butterknife in your kitchen
+	//	put two slices of bread on a plate
+	//	open the peanut butter and jelly jars
+	//	use your butterknife and scoop out some peanut butter 
+	//	spread peanut butter generously on one piece of bread
+	//	clean off your butterknife 
+	//	use your butterknife and scoop out some jelly 
+	//	spread jelly generously on the other piece of bread
+	//	clean off your butterknife again
+	//	close lids of peanut butter and jelly jars
+	//	put the rest of the loaf and the peanut butter back in the pantry
+	//	put the jelly back in the fridge
+	//	put the two sides of bread together, so that the peanut butter and jelly are touching
+	//	take your clean butterknife and slice diagonally through the sandwich
+	//	take a bite and enjoy :)
